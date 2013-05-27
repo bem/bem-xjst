8b76f70c80a44cadf9d13bd5c48f0059805aa671
Fedor Indutny <fedor.indutny@gmail.com>
2013-05-27 16:25:08 +0400

    compiler: cache should work with __$history

---
+++ test/cache-test.js
@@ -0,0 +1,44 @@
+var bem = require('..');
+var assert = require('assert');
+var utile = require('utile');
+
+describe('BEM.js cache', function() {
+  function test(fn, data, expected, options) {
+    if (!options) options = {};
+
+    var body = (options.ibem !== false ? require('./fixtures/i-bem') : '') +
+               ';\n' +
+               fn.toString().replace(/^function\s*\(\)\s*{|}$/g, '');
+    var fns = [
+      bem.compile(body, utile.mixin({}, options, { cache: true }))
+    ];
+
+    fns.forEach(function(fn) {
+      assert.deepEqual(fn.apply.call(data || {}), expected);
+    });
+  }
+
+  it('should work with locals', function() {
+    test(function() {
+      match()(function() {
+        return local('mode', { x: 1, y: this.x.y, z: this[false] })(function() {
+          return this.__$history
+        });
+      });
+    }, { __$history: [] }, [
+      ['_mode', 'mode'],
+      ['x',1],
+      ['y','x.y',1]
+    ], { ibem: false });
+  });
+
+  it('should work without history', function() {
+    test(function() {
+      match()(function() {
+        return local('mode', { x: 1 })(function() {
+          return 'ok';
+        });
+      });
+    }, {}, 'ok', { ibem: false });
+  });
+});
