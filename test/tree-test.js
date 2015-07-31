20a94f45ea68100679b75c124b7f52782f526be4
Fedor Indutny <fedor@indutny.com>
2015-07-30 17:21:38 -0700

    match: fix early `once` / `wrap`

--- test/tree-test.js
+++ test/tree-test.js
@@ -238,6 +238,26 @@ describe('BEMHTML compiler/Tree', function() {
     });
   });
 
+  it('should support early `.once()`', function() {
+    test(function() {
+      block('b1').content()(function() {
+        return 'second';
+      });
+      block('b1').once().match(function() {
+        return !this.ctx.early;
+      }).content()(function() {
+        return 'first';
+      });
+    }, [ {
+      block: 'b1',
+      early: true
+    }, {
+      block: 'b1'
+    } ], '<div class="b1">second</div><div class="b1">first</div>', {
+      count: 5
+    });
+  });
+
   it('should support `.wrap()`', function() {
     test(function() {
       block('b1').wrap().def()(function() {
