d469902e927ab9a917361d189a4026d062117efc
Fedor Indutny <fedor@indutny.com>
2015-08-18 13:58:03 -0700

    runtime: use ClassBuilder everywhere
    Fix: #55

--- test/runtime-test.js
+++ test/runtime-test.js
@@ -743,16 +743,28 @@ describe('BEMHTML compiler/Runtime', function() {
   describe('naming', function() {
     it('should support custom naming', function() {
       test(function() {
-      }, {
-        block: 'b1',
-        elem: 'e1',
-        mods: {
-          a: 'b'
+      }, [
+        {
+          block: 'b1',
+          elem: 'e1',
+          mods: {
+            a: 'b'
+          }
+        },
+        {
+          block: 'b1',
+          elem: 'e1'
+        },
+        {
+          block: 'b1',
+          mix: { elem: 'e2' }
         }
-      }, '<div class="b1__e1 b1__e1--a--b"></div>', {
+      ], '<div class="b1$$e1 b1$$e1@@a@@b"></div>' +
+         '<div class="b1$$e1"></div>' +
+         '<div class="b1 b1$$e2"></div>', {
         naming: {
-          elem: '__',
-          mod: '--'
+          elem: '$$',
+          mod: '@@'
         }
       });
     });
