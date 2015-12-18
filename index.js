199c068909733854d3df614f6ca046b3e75e62cd
Vladimir Grinenko <i@tadatuta.com>
2015-12-19 02:47:53 +0300

    index.js: More efficient usage of cache

--- index.js
+++ index.js
@@ -3,13 +3,15 @@ var Compiler = require('./lib/compiler').Compiler;
 var _cache = {};
 
 function getEngine(engineName) {
+  if (_cache[engineName]) return _cache[engineName];
+
   var runtime = require('./lib/' + engineName);
   var pathToBundle = require.resolve('./lib/' + engineName + '/bundle');
   var sourceBundle = fs.readFileSync(pathToBundle, 'utf8');
 
   runtime.source = sourceBundle;
 
-  return _cache[engineName] || (_cache[engineName] = new Compiler(runtime));
+  return _cache[engineName] = new Compiler(runtime);
 }
 
 module.exports = {
