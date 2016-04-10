3703c12ef99d050a5f2d9565935e4a506ce923d7
amel <amel.truelife@gmail.com>
2016-04-10 16:38:49 +0300

    Refactoring of demo.js

--- common.blocks/demo/demo.js
+++ common.blocks/demo/demo.js
@@ -9,13 +9,18 @@ modules.define('demo', [ 'i-bem__dom', 'pretty', 'functions__debounce' ], functi
                     this._engineSelector = this._page.findBlockInside('engine-selector');
                     this._$engineSelector = this._engineSelector.domElem;
 
-                    var _this = this;
                     this.params.engine = this._$engineSelector.find('option:selected').val();
-                    this._$engineSelector.on('change', function() {
-                        _this.params.engine = _this._$engineSelector.find('option:selected').val();
-                        _this._render();
-                    });
 
+                    this._$engineSelector.on('change', function() {
+                        this.params.engine = this._$engineSelector.find('option:selected').val();
+                        this._render();
+                    }.bind(this));
+
+                    this._engines = {
+                        vidom: vidom,
+                        bemhtml: BEMHTML,
+                        bemjson: {}
+                    };
 
                     this._bemhtml = this.findBlockOn('bemhtml', 'editor');
                     this._bemjson = this.findBlockOn('bemjson', 'editor');
@@ -38,6 +43,51 @@ modules.define('demo', [ 'i-bem__dom', 'pretty', 'functions__debounce' ], functi
             }
         },
 
+        _getEngine: function(engineName, engines) {
+            var bemhtml = {};
+            var compiledText = '';
+            var _engine = engines[engineName];
+            return {
+                render: function (bemjson) {
+                    var BEMJSON = safeEval(bemjson);
+
+                    if (BEMJSON instanceof Error) {
+                        return 'BEMJSON error: ' + BEMJSON.message + '\n' + BEMJSON.stack;
+                    }
+
+                    try {
+                        switch (engineName) {
+                            case 'vidom':
+                            {
+                                var api = new _engine({});
+                                api.compile(bemjson);
+                                api.exportApply(bemhtml);
+                                compiledText = JSON.stringify(bemhtml.apply(BEMJSON), null, 4);
+                                break;
+                            }
+                            case 'bemhtml':
+                            {
+                                var api = new _engine({});
+                                api.compile(bemjson);
+                                api.exportApply(bemhtml);
+                                compiledText = pretty(bemhtml.apply(BEMJSON), {max_char: 1000});
+                                break;
+                            }
+                            case 'bemjson':
+                            {
+                                compiledText = JSON.stringify(BEMJSON, null, 4);
+                                break;
+                            }
+                        }
+                    } catch (e) {
+                        return engineName.toUpperCase() + ' error: ' + e.message + '\n' + e.stack;
+                    }
+                    return compiledText;
+                }
+            }
+
+        },
+
         _onChange: function() {
             this._render();
             this._save();
@@ -49,45 +99,8 @@ modules.define('demo', [ 'i-bem__dom', 'pretty', 'functions__debounce' ], functi
             return this._bemjson.getValue();
         },
         _render: function() {
-            try {
-                var bemhtml = {};
-
-                var api = (this.params.engine === 'vidom')
-                    ? new vidom({})
-                    : new BEMHTML({});
-                api.compile(this._getBEMHTML());
-                api.exportApply(bemhtml);
-            } catch(e) {
-                this._html.setValue('BEMHTML error: ' + e.message + '\n' + e.stack);
-                return;
-            }
-
-            var BEMJSON = safeEval(this._getBEMJSON());
-
-            if (BEMJSON instanceof Error) {
-                this._html.setValue('BEMJSON error: ' + BEMJSON.message + '\n' + BEMJSON.stack);
-                return;
-            }
-
-            var compiledText = '';
-
-            switch(this.params.engine) {
-                case 'vidom':{
-                    compiledText = JSON.stringify(bemhtml.apply(BEMJSON), null, 4);
-                    break;
-                }
-                case 'bemhtml':{
-                    compiledText = pretty(bemhtml.apply(BEMJSON), { max_char: 1000});
-                    break;
-                }
-                case 'bemjson':{
-                    compiledText = JSON.stringify(BEMJSON, null, 4);
-                    break;
-                }
-            }
-
-
-            this._html.setValue(compiledText);
+            var engine = this._getEngine(this.params.engine, this._engines);
+            this._html.setValue(engine.render(this._getBEMJSON()));
         },
         _save: function() {
             var bemhtml = this._getBEMHTML(),
