ffb3e362525523aeac74fe54275bae94a6e2853a
miripiruni <miripiruni@gmail.com>
2016-07-13 17:49:33 +0300

    Semantic: change behaviour of `mix`, `js` и `attrs` modes

--- test/modes-js-test.js
+++ test/modes-js-test.js
@@ -27,28 +27,12 @@ describe('Modes js', function() {
     '<div class="button"></div>');
   });
 
-  it('should not override user declarations', function() {
+  it('should override user declarations', function() {
     test(function() {
       block('button').js()(true);
     },
     { block: 'button', js: false },
-    '<div class="button"></div>');
-  });
-
-  it('should extend user js', function() {
-    test(function() {
-      block('button').js()({ a: 2 });
-    },
-    { block: 'button', js: { x: 1 } },
-    '<div class="button i-bem" data-bem=\'{"button":{"x":1,"a":2}}\'></div>');
-  });
-
-  it('should extend user js, when it equal true', function() {
-    test(function() {
-      block('button').js()({ a: 1 });
-    },
-    { block: 'button', js: true },
-    '<div class="button i-bem" data-bem=\'{"button":{"a":1}}\'></div>');
+    '<div class="button i-bem" data-bem=\'{"button":{}}\'></div>');
   });
 
   it('should not override later declarations #1', function() {
@@ -78,12 +62,11 @@ describe('Modes js', function() {
     { elemJsInstances: true });
   });
 
-  it('should merge js from templates and js from bemjson', function() {
+  it('should override js from bemjson via js from templates', function() {
     test(function() {
       block('b').js()({ templ: '1' });
     },
     { block: 'b', js: { bemjson: '2' } },
-    '<div class="b i-bem" data-bem=\'{"b":{"bemjson":' +
-      '"2","templ":"1"}}\'></div>');
+    '<div class="b i-bem" data-bem=\'{"b":{"templ":"1"}}\'></div>');
   });
 });
