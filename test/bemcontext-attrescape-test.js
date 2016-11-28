1977ee83dad260aad72295325b5b3257f7b028c6
Vasiliy Loginevskiy <yeti-or@yandex-team.ru>
2016-11-29 02:01:01 +0300

    Utils: escaping function should’t render undefined/Null/NaN
    Motivation: for safety reasons I decided to change regular cast to string. In
    frontend we need only useful values such as Strings and Numbers. All undefined,
    NaN, Null and [Object object] values is useless and are a mistake in HTML.

--- test/bemcontext-attrescape-test.js
+++ test/bemcontext-attrescape-test.js
@@ -1,4 +1,5 @@
 var fixtures = require('./fixtures')('bemhtml');
+var compile = fixtures.compile;
 var test = fixtures.test;
 
 describe('BEMContext this.attrEscape(str)', function() {
@@ -10,5 +11,68 @@ describe('BEMContext this.attrEscape(str)', function() {
     },
     { block: 'button' },
     '<b id=&quot;a&quot; class=&quot;bem&quot;>a&amp;b&amp;c</b>');
+  });
+
+  describe('Type of argument', function() {
+    var bemhtml;
+
+    before(function() {
+      bemhtml = compile(function() {
+        block('b').def()(function(n, ctx) {
+          return n.attrEscape(ctx.val);
+        });
+      });
+    });
+
+    it('should return \'\' for undefined', function() {
+      bemhtml.apply({ block: 'b', val: undefined }).should.equal('');
+    });
+
+    it('should return \'\' for null', function() {
+      bemhtml.apply({ block: 'b', val: null }).should.equal('');
+    });
+
+    it('should return String for zero', function() {
+      bemhtml.apply({ block: 'b', val: 0 }).should.equal('0');
+    });
+
+    it('should return String for Number', function() {
+      bemhtml.apply({ block: 'b', val: 42 }).should.equal('42');
+    });
+
+    it('should return \'\' for NaN', function() {
+      bemhtml.apply({ block: 'b', val: NaN }).should.equal('');
+    });
+
+    it('should return String for String', function() {
+      bemhtml.apply({ block: 'b', val: '' }).should.equal('');
+    });
+
+    it('should return String for String', function() {
+      bemhtml.apply({ block: 'b', val: 'test' }).should.equal('test');
+    });
+
+    it('should return String for Boolean', function() {
+      bemhtml.apply({ block: 'b', val: false }).should.equal('false');
+    });
+
+    it('should return String for Array', function() {
+      bemhtml.apply({ block: 'b', val: [] }).should.equal('');
+    });
+
+    it('should return String for Array', function() {
+      bemhtml.apply({ block: 'b', val: [ 'a', 'b' ] }).should.equal('a,b');
+    });
+
+    it('should return \'\' for Object', function() {
+      bemhtml.apply({ block: 'b', val: {
+        toString: function() { return ''; }
+      } }).should.equal('');
+    });
+
+    it('should return \'\' for Function', function() {
+      bemhtml.apply({ block: 'b', val: function() {} })
+        .should.equal('function () {}');
+    });
   });
 });
