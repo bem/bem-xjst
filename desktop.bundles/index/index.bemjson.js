e42ccf11b9336cc71f9ac8f8445e6f0dd4afb6fa
miripiruni <miripiruni@gmail.com>
2017-01-25 14:25:12 +0300

    8.5.1 in version selector

--- desktop.bundles/index/index.bemjson.js
+++ desktop.bundles/index/index.bemjson.js
@@ -44,6 +44,7 @@ module.exports = {
                             mix: { block: 'header', elem: 'item' },
                             mods: { state: 'loading' },
                             versions: [
+                                { name: "8.5.1", hash: "68234af4fc284881d94b765a17c77050a4a45238" },
                                 { name: "8.5.0", hash: "0140ceb2bd2e514e6c686d79195f0cbd11f2d49a" },
                                 { name: "8.4.2", hash: "51f2d98e1f2df1e8101d1e61b7b80bb9437cf772" },
                                 { name: "8.4.0", hash: "50b78619734d5d15d00b7bcc4c4633f5a55f0292" },
