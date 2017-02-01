687969fb1429439ca7ff9fd038f0df82493f5890
Slava Oliyanchuk <miripiruni@gmail.com>
2017-02-01 23:25:34 +0300

    Use bem-xjst in Node.js without bundling via browserify (#407 and #408 fixed)

--- index.js
+++ index.js
@@ -1,18 +1,12 @@
-var fs = require('fs');
-var Compiler = require('./lib/compiler').Compiler;
+var Compiler = require('./lib/compiler');
 var _cache = {};
 
 function getEngine(engineName) {
-  if (_cache[engineName]) return _cache[engineName];
+  var engine = _cache[engineName];
 
-  var runtime = require('./lib/' + engineName);
-  var pathToBundle = require.resolve('./lib/' + engineName + '/bundle');
-  var sourceBundle = fs.readFileSync(pathToBundle, 'utf8');
+  return engine || (engine = new Compiler(engineName));
+};
 
-  runtime.source = sourceBundle;
-
-  return _cache[engineName] = new Compiler(runtime);
-}
 
 module.exports = {
   get bemtree() { return getEngine('bemtree'); },
