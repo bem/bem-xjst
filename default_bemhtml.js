7bfdd8bbfe0e888453799869bfd3a9f4dd8d48df
miripiruni <miripiruni@gmail.com>
2018-04-03 17:38:55 +0300

    new default templates (with shortcut syntax)

--- default_bemhtml.js
+++ default_bemhtml.js
@@ -1,23 +1,27 @@
+// Default tag
+block('link')({ tag:'span' });
+
 block('link')(
-  // Default tag
-  tag()('span'),
+    // If url exists in BEMJSON
+    match((context, json) => json.url)({
+        // Then set a tag
+        tag: 'a',
 
-  // If url exists in BEMJSON
-  match(function (context, json) { return json.url; })(
-    // Then set a tag
-    tag()('a'),
-    // and set href HTML attribute
-    addAttrs()((context, json) => ({ href: json.url }))
-    // P.S. Arrow functions are supported as you can see
-  ),
+        // and set href HTML attribute
+        addAttrs: (context, json) => ({ href: json.url })
+    })
+),
 
-  // If target blank exists in BEMJSON
-  match((context, json) => json.target === '_blank')
-    // add rel attribute
-    .addAttrs()({ rel: 'noopener' }),
+block('link')(
+    // If target blank exists in BEMJSON
+    match((context, json) => json.target === '_blank')({
+        // add rel attribute
+        addAttrs: { rel: 'noopener' }
+    }),
 
-  // If "disabled" modifier exist
-  match(context => context.mods.disabled)
-    // add aria attribute
-    .addAttrs()({ 'aria-disabled': 'true' })
+    // If "disabled" modifier exist
+    match(context => context.mods.disabled)({
+        // add aria attribute
+        addAttrs: { 'aria-disabled': 'true' }
+    })
 );
