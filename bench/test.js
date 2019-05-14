1d0db55afcf5b1ceb7722f71b89ef8069aaa3de1
v-homyakov <v-homyakov@yandex-team.ru>
2019-05-14 18:00:02 +0300

    Updated benchmarking code and documentation
    - added folders with benchmark data to .gitignore
    - updated output of old benchmark script
    - updated code used to build histogram
    - updated documentation
    - loop over bemjson files when actual number of files is less than
      value specified in --bemjson parameter

--- bench/test.js
+++ bench/test.js
@@ -1,33 +1,36 @@
 'use strict';
 const fs = require('fs');
+
 const argv = require('yargs')
-    .describe('rev', 'bem-xjst revision (commit hash)')
-    .describe('bemjson', 'amount of bemjson files (default 2000, max 2000)')
-    .describe('verbose', 'verbose results')
-    .describe('dataPath', 'path to bemjson files')
-    .describe('templatePath', 'path to template file')
-    .help('h')
-    .alias('help', 'h')
-    .argv;
+  .describe('rev', 'bem-xjst revision (commit hash)')
+  .describe('bemjson', 'amount of bemjson files to render (default 2000)')
+  .describe('dataPath', 'path to bemjson files')
+  .describe('templatePath', 'path to template file')
+  .describe('verbose', 'verbose results')
+  .help('h')
+  .alias('help', 'h')
+  .argv;
+
 const dataPath = argv.dataPath || './node_modules/web-data/data';
+const templatePath = argv.templatePath || './node_modules/web-data/templates.js';
 const files = fs.readdirSync(dataPath);
 const times = argv.bemjson || 2000;
 const secInNs = 1e9;
 const res = [];
 
 const bemhtml = require('./bem-xjst-' + argv.rev).bemhtml;
-const templates = bemhtml.compile(fs.readFileSync(argv.templatePath || './node_modules/web-data/templates.js', 'utf8'), { escapeContent: true });
+const templates = bemhtml.compile(fs.readFileSync(templatePath, 'utf8'), { escapeContent: true });
 
 for (var i = 0; i < times; i++) {
-    var path = dataPath + '/' + files[i];
-    //argv.verbose && console.warn(path);
-    var data = JSON.parse(fs.readFileSync(path, 'utf8'));
+  var path = dataPath + '/' + files[i % files.length];
+  // argv.verbose && console.warn(path);
+  var data = JSON.parse(fs.readFileSync(path, 'utf8'));
 
-    var time = process.hrtime();
-    templates.apply(data);
-    var diff = process.hrtime(time);
+  var time = process.hrtime();
+  templates.apply(data);
+  var diff = process.hrtime(time);
 
-    res.push((diff[0] * secInNs + diff[1]) / 1000000);
+  res.push((diff[0] * secInNs + diff[1]) / 1000000);
 }
 
 console.log(JSON.stringify(res));
